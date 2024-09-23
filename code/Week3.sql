9d78e0ec30bf81adfd73cba5878f28c33c1d0b54
swallow02 <93142981+swallow02@users.noreply.github.com>
2024-09-23 21:18:13 +0900

    Update Week3.sql

--- code/Week3.sql
+++ code/Week3.sql
@@ -1,3 +1,21 @@
+-- CREATE DATABASE djudb;
+
+
+CREATE TABLE teachers(
+id bigserial,
+first_name varchar(25),
+last_name varchar(50),
+school varchar(50),
+hire_date date,
+salary numeric
+);
+
+INSERT INTO teachers(first_name,last_name,school,hire_date,salary)
+VALUES ('Janey','Smith','F.D.Roosevelt H.S.','2011-10-30',36200),
+		('LEee','Reynolds','F.D.Roosevelt H.S.','1993-05-22',65000),
+		('Samuel','Cole','Meyers M.S.','2005-08-01',435500);
+SELECT * from teachers;
+
 -- Week 3 Code --------------------------------
 
 SELECT last_name,school, salary FROM teachers;
