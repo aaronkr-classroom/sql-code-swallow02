a3d796edcbc191501e96fcfedbf24083d93a32cb
swallow02 <93142981+swallow02@users.noreply.github.com>
2024-11-05 11:24:55 +0900

    Add files via upload

---
+++ code/week10.sql
@@ -0,0 +1,15 @@
+SELECT * FROM 학생;
+
+-- 트랜잭션
+-- BEGIN; 또는 BEGIN WORK; 또는 BEGIN TRANSACTION: (postgreSQL)
+BEGIN; --STArT TRANSATION; --MySQL
+DELETE FROM 학생 WHERE 학생.성별='남';
+DELETE FROM 학생 WHERE 학생.성별='여';
+SELECT * FROM 학생;
+ROLLBACK; --또는 ROLLBACK WORK; 또는 ROLLBACK TRANSACTION; 취소하기
+SELECT * FROM 학생;
+
+BEGIN;
+UPDATE 학생 SET 이름 = '홍길순' WHERE 학번 = 's002';
+SELECT * FROM 학생;
+COMMIT; --저장하기
